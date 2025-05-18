b0fdc89d1de147928ac47729bbec8c85ee9ac584
Ahbideen Yusuf <ahbideeny@gmail.com>
2025-05-18 11:51:30 +0100

    Refactor MoviesTable component to use functional component syntax and Next.js Link

--- components/moviesTable.jsx
+++ components/moviesTable.jsx
@@ -1,62 +1,60 @@
-import React, { Component } from "react";
-import auth from "../services/authService";
-import { Link } from "react-router-dom";
+import React from "react";
+import Link from "next/link";
 import Table from "./common/table";
 import Like from "./common/like";
 
-class MoviesTable extends Component {
-  columns = [
+const MoviesTable = ({
+  movies,
+  onSort,
+  sortColumn,
+  onLike,
+  onDelete,
+  user,
+}) => {
+  const columns = [
     {
       path: "title",
       label: "Title",
       content: (movie) => (
-        <Link to={`/movies/${movie._id}`}>{movie.title}</Link>
+        <Link href={`/movies/${movie._id}`}>{movie.title}</Link>
       ),
     },
     { path: "genre.name", label: "Genre" },
     { path: "numberInStock", label: "Stock" },
     { path: "dailyRentalRate", label: "Rate" },
-
     {
       key: "like",
       content: (movie) => (
-        <Like liked={movie.liked} onLike={() => this.props.onLike(movie)} />
+        <Like liked={movie.liked} onLike={() => onLike(movie)} />
       ),
     },
   ];
 
-  deleteColumn = {
-    key: "delete",
-    content: (movie) => (
-      <button
-        onClick={() => this.props.onDelete(movie)}
-        className="btn btn-danger btn-sm"
-      >
-        Delete
-      </button>
-    ),
-  };
-
-  constructor() {
-    super();
-    const user = auth.getCurrentUser();
-    if (user && user.isAdmin) this.columns.push(this.deleteColumn);
+  // Only show delete button if user is present (optional)
+  if (user) {
+    columns.push({
+      key: "delete",
+      content: (movie) => (
+        <button
+          onClick={() => onDelete(movie)}
+          className="btn btn-danger btn-sm"
+        >
+          Delete
+        </button>
+      ),
+    });
   }
 
-  render() {
-    const { movies, onSort, sortColumn } = this.props;
-
-    return (
-      <div>
-        <Table
-          columns={this.columns}
-          data={movies}
-          sortColumn={sortColumn}
-          onSort={onSort}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Table
+        columns={columns}
+        data={movies}
+        sortColumn={sortColumn}
+        onSort={onSort}
+      />
+    </div>
+  );
+};
 
 export default MoviesTable;
