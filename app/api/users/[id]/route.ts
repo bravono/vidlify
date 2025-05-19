0ac40e72cb44f0130b44885b8bcacf1e4f7359f1
Ahbideen Yusuf <ahbideeny@gmail.com>
2025-05-19 13:44:32 +0100

    Refactor user route to improve type safety and error handling

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -1,21 +1,31 @@
-import { NextResponse } from "next/server";
 import dbConnect from "../../../../lib/mongodb";
 import { User } from "../../../../lib/models/User";
+import { NextRequest, NextResponse } from "next/server";
+
+interface Params {
+  params: {
+    id: string;
+  };
+}
+
+interface ErrorResponse {
+  error: string;
+}
 
 export async function GET(
-  
-  { params }: { params: { id: string } }
-) {
+  req: NextRequest,
+  { params }: Params
+): Promise<NextResponse<any>> {
   try {
     await dbConnect();
     const user = await User.findById(params.id);
     if (!user)
-      return NextResponse.json({ error: "User not found" }, { status: 404 });
+      return NextResponse.json<ErrorResponse>({ error: "User not found" }, { status: 404 });
     return NextResponse.json(user);
   } catch (error) {
-        console.log(error)
+    console.log(error);
 
-    return NextResponse.json(
+    return NextResponse.json<ErrorResponse>(
       { error: "Failed to fetch user" },
       { status: 500 }
     );
